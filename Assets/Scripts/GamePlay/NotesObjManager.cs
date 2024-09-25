43724cdcc1ef6bb5eabcebab7a9594e575e8b6cc
bbbasya <42473270+asyaB404@users.noreply.github.com>
2024-09-25 20:49:39 +0800

    场景准备

--- Assets/Scripts/GamePlay/NotesObjManager.cs
+++ Assets/Scripts/GamePlay/NotesObjManager.cs
@@ -5,6 +5,7 @@ namespace GamePlay
 {
     public class NotesObjManager : MonoManager<NotesObjManager>
     {
+        [SerializeField] private GameObject notePrefabs;
         [SerializeField] private List<int> curNotesGobjIndexList = new(ChartManager.KeysCount);
     }
 }
